6fcba0b401fc144333e9ecca22e925e7a541d1ef
srinipashi <108763976+srinipashi@users.noreply.github.com>
2022-07-05 18:22:23 -0400

    Update streamlit_app.py
    Only selected fruits to show in the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,16 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),default=['Avocado','Strawberries'])
-#streamlit.multiselect("Pick some fruits:", ['Avocado','Strawberries'])
-
+# Below is to show all the fruits and show default 2 ['Avocado','Strawberries']
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),default=['Avocado','Strawberries'])
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
 
+#Below is to show default and selected fruits into a variable fruits_selected and show only those which are in the selected list
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),default=['Avocado','Strawberries'])
+fruits_toshow = fruits_list.loc[fruits_selected]
+#Display the table on the app with select fruits only
+streamlit.dataframe(fruits_toshow)
 
 
 
