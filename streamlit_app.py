775e895418763ed5285af0dce85afb9f86e9c430
srinipashi <108763976+srinipashi@users.noreply.github.com>
2022-07-12 13:04:37 -0400

    Update streamlit_app.py
    Added connection close command my_cnx.close() in two functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,16 +92,18 @@ except URLError as e:
   #streamlit.dataframe(my_data_row)
 # Orginal Block 2 End
 # New Block 2
-streamlit.header("The fruit load list contains:")
+#streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #Snowflake related function
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 #Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 # End of new Block 2
 ## Don't run anything past here while we troubleshoot
@@ -126,6 +128,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
 # New Block 3
 
