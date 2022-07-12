aefa02beb7a49767c3fbb440608e3b0b598f56a0
srinipashi <108763976+srinipashi@users.noreply.github.com>
2022-07-12 00:24:58 -0400

    Update streamlit_app.py
    Running some valid queries than current_user current account region

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,9 @@ import snowflake.connector
 ## This is running snowflake data from streamlit
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+##my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+##streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 streamlit.text(my_data_row)
