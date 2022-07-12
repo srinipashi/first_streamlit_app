e3123cb7759fcef6e8fc595f8e271a3573975665
srinipashi <108763976+srinipashi@users.noreply.github.com>
2022-07-12 10:03:24 -0400

    Update streamlit_app.py
    Added insert statement for testing purpose

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,3 +57,6 @@ streamlit.dataframe(my_data_row)
 ## allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding: ',add_my_fruit)
+
+## Trying to insert a row into a table
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')");
